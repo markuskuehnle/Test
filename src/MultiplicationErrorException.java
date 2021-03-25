b3f97bf3b2438f2e04a2a67ae95c0a81456b739e
markuskuehnle <81199090+markuskuehnle@users.noreply.github.com>
2021-03-25 16:23:14 +0100

    Delete MultiplicationErrorException.java

--- src/MultiplicationErrorException.java
+++
@@ -1,9 +0,0 @@
-public class MultiplicationErrorException extends Exception {
-    public MultiplicationErrorException() {
-        super("Multiplikation nicht möglich!");
-    }
-
-    public MultiplicationErrorException(String fehler) {
-        super(fehler);
-    }
-}
